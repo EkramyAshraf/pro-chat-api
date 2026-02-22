8a1dbf735a6def153fe145a52ba39ff710d09a56
Ekramy Ashraf <ekramyashraf@gmail.com>
2026-02-22 17:48:47 +0200

    feat: link socket io with http server

--- src/app.js
+++ src/app.js
@@ -15,12 +15,12 @@ const app = express();
 // Body parser, reading data from body into req.body
 app.use(cors());
 app.use(express.json({ limit: "20kb" }));
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: "*",
-//   },
-// });
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 app.get("/", (req, res) => {
   res.send("server is running");
 });
@@ -41,14 +41,8 @@ app.use("/api/auth", authRoutes);
 // app.use("/api/conversations", conversationRoutes);
 
 //Socket.io basic connection
-// io.on("connection", (socket) => {
-//   console.log("New WS connection: ", socket.id);
-
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected");
-//   });
-// });
+require("./sockets/chat")(io);
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
---
+++ src/sockets/chat.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+module.exports = (io) => {
+  //auth middleware
+  io.use((socket, next) => {
+    const token = socket.handshake.auth?.token || socket.handshake.query?.token;
+    if (!token) {
+      return next(new Error("Authentication error: Token missing"));
+    }
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      socket.userId = decoded.id;
+      next();
+    } catch (err) {
+      next(new Error("Authentication error: Invalid token"));
+    }
+  });
+  io.on("connection", (socket) => {
+    console.log(
+      `✅ User Authorized: ${socket.userId} (Socket ID: ${socket.id})`,
+    );
+
+    socket.on("disconnect", () => {
+      console.log("❌ User disconnected");
+    });
+  });
+};
