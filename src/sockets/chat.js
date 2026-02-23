56e7860af9c9321684f645914dd5dc0996d9a016
Ekramy Ashraf <ekramyashraf@gmail.com>
2026-02-23 16:52:31 +0200

    feat: implement socket authentication and private message persistence

--- src/sockets/chat.js
+++ src/sockets/chat.js
@@ -1,14 +1,17 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+const Message = require("../models/messageModel");
 module.exports = (io) => {
   //auth middleware
-  io.use((socket, next) => {
+  io.use(async (socket, next) => {
     const token = socket.handshake.auth?.token || socket.handshake.query?.token;
     if (!token) {
       return next(new Error("Authentication error: Token missing"));
     }
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      socket.userId = decoded.id;
+      const user = await User.findById(decoded.id);
+      socket.user = user;
 
       next();
     } catch (err) {
@@ -17,9 +20,27 @@ module.exports = (io) => {
   });
 
   io.on("connection", (socket) => {
-    console.log(
-      `✅ User Authorized: ${socket.userId} (Socket ID: ${socket.id})`,
-    );
+    console.log(`✅ User connected: ${socket.user.name}`);
+
+    socket.join(socket.user._id.toString());
+    try {
+      socket.on("send_message", async (data) => {
+        const { receiverId, message } = data;
+        const newMessage = await Message.create({
+          sender: socket.user._id,
+          receiver: receiverId,
+          message: message,
+        });
+        io.to(receiverId).emit("receive_message", {
+          senderId: socket.user._id,
+          message: message,
+          timestamp: newMessage.createdAt,
+          _id: newMessage._id,
+        });
+      });
+    } catch (error) {
+      console.error("Save Error:", error);
+    }
 
     socket.on("disconnect", () => {
       console.log("❌ User disconnected");
